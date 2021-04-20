68108c406cc95c969fbaf9c27dd3831e4c626b6e
cedretaber <cedretaber@gmail.com>
2021-04-20 17:15:19 +0900

    Alter the output format.

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default [
     input: "src/index.ts",
     output: {
       file: "dist/index.js",
-      format: "es",
+      format: "iife",
     },
     plugins: [typescript({ target: "ESNext" })],
   },
